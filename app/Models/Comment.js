8b5be743938fb5bdbb78c2ef6b1acb323d98e448
PHENIXCRIME <photboatxeemus@icloud.com>
2020-09-13 02:23:21 +0700

    ADD fk model

--- app/Models/Comment.js
+++ app/Models/Comment.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const Blog = require('./Blog')
+
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
 
@@ -8,6 +10,16 @@ class Comment extends Model {
     static get primaryKey() {
         return 'comment_id'
     }
+
+    user() {
+        return this.belongsTo("App/Models/User")
+    }
+
+    blog() {
+        return this.belongsTo("App/Models/Blog")
+    }
+
+
     static get createdAtColumn() {
         return null
     }
