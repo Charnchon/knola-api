cc5078cb3284a33245ef72c2e6da05fed541425a
PHENIXCRIME <photboatxeemus@icloud.com>
2020-09-12 21:02:46 +0700

    CREATE Model Tag

---
+++ app/Models/Tag.js
@@ -0,0 +1,25 @@
+'use strict'
+
+/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
+const Model = use('Model')
+
+class Tag extends Model {
+    static get primaryKey() { 
+        return 'tag_id'
+    }
+
+    static get createdAtColumn() {
+        return null
+    }
+
+    static get updatedAtColumn() {
+        return null
+    }
+
+    blog() { 
+        return this.hasMany("App/Models/Blog")
+    }
+    
+}
+
+module.exports = Tag
