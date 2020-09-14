67638212e79abe278f105ad30c4bae87b776ad2b
Charnchon <boatcharnchon@gmail.com>
2020-09-15 00:33:40 +0700

    UPDATE userUtil test

--- test/unit/user-util.spec.js
+++ test/unit/user-util.spec.js
@@ -9,13 +9,13 @@ test("should get more than one when get multiple users", async ({ assert }) => {
     assert.isAbove(users.rows.length, 1)
 })
 
-test("should return object of requested created index from UserUtil.", async ({assert,}) => {
+test("should return object of requested created index from UserUtil.", async ({assert}) => {
     const user =  new UserUtil(User)
     const {user_id} = await user.getById(2)
     assert.isOk(user)
 })
   
-test("should return structured data with no references via post method.", async ({client,}) => {
+test("should return structured data with no references via post method", async ({client,}) => {
     const user = {
         user_first_name : 'John',
         user_last_name: 'Doe',
@@ -30,7 +30,7 @@ test("should return structured data with no references via post method.", async 
     await User.find(response.body.data.user_id).then((response) =>response.delete())
 })
 
-test("should return structured data with no references via put method.", async ({client,}) => {
+test("should return structured data with no references via put method", async ({client,}) => {
     const user1 = {
         user_first_name : 'John',
         user_last_name: 'Doe',
@@ -40,13 +40,33 @@ test("should return structured data with no references via put method.", async (
         user_password: '123456789',
     }
     const response1 = await client.post('http://127.0.0.1:4000/api/v1/users').send(user1).end()
-    console.log(response1.body.data.user_id)
     const user2 = {
         user_first_name : 'John',
         user_last_name: 'Doe'
     }
-    const response2 = await client.put(`http://127.0.0.1:4000/api/v1/users/${response1.body.data.user_id}`).send(user2).end()
+    const response2 = await client.put(`http://127.0.0.1:4000/api/v1/users/${response1.body.data.user_id}`)
+        .send(user2).end()
     response2.assertStatus(200)
     response2.assertJSONSubset({error: undefined,})
     await User.find(response2.body.data.user_id).then((response) =>response.delete())
+})
+
+test("should return structured data with no references via delete method", async ({client,assert}) => {
+    const user1 = {
+        user_first_name : 'John',
+        user_last_name: 'Doe',
+        user_age : '18',
+        user_email: 'johndoeunqiue@gmail.com',
+        user_username: 'johndoe001unqiue',
+        user_password: '123456789',
+    }
+    const response1 = await client.post('http://127.0.0.1:4000/api/v1/users').send(user1).end()
+    const userId = await response1.body.data.user_id
+    const user2 =  new UserUtil(User)
+    const {user_id} = await user2.getById(userId)
+    const response2 = await client.delete(`http://127.0.0.1:4000/api/v1/users/${response1.body.data.user_id}`)
+        .end()
+    response2.assertStatus(200)
+    response2.assertJSONSubset({error: undefined})
+    assert.isNotOk(response2.body.data.user_id)
 })
