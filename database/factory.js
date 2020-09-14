8b6b53635bfd5d493268176002edf467d44216a4
PHENIXCRIME <photboatxeemus@icloud.com>
2020-09-14 13:44:41 +0700

    EDIT blueprint  faker user_id & blog_id

--- database/factory.js
+++ database/factory.js
@@ -45,6 +45,8 @@ Factory.blueprint("App/Models/Blog" , (faker) => {
 
 Factory.blueprint("App/Models/Comment" , (faker) => { 
     return { 
-        comment_content : faker.paragraph({ sentences : 1 })
+        comment_content : faker.paragraph({ sentences : 1 }) , 
+        user_id : faker.integer({min : 1 , max : 10}) ,
+        blog_id : faker.integer({min : 1 , max : 10 }) 
     }    
 })
