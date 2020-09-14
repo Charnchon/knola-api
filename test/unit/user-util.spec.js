1340d2760bf0fb2627b75d20e8a05911e1af8414
Charnchon <boatcharnchon@gmail.com>
2020-09-14 23:32:05 +0700

    ADD userUtil test

--- test/unit/user-util.spec.js
+++ test/unit/user-util.spec.js
@@ -1,6 +1,7 @@
 const UserUtil = require("../../util/userUtil")
 const User = use('App/Models/User')
-const {test} = use('Test/Suite')('Example')
+const { test, trait } = use("Test/Suite")("User Controller")
+trait("Test/ApiClient")
 
 test("should get more than one when get multiple users", async ({ assert }) => {
     const userUtil = new UserUtil(User);
@@ -10,8 +11,42 @@ test("should get more than one when get multiple users", async ({ assert }) => {
 
 test("should return object of requested created index from UserUtil.", async ({assert,}) => {
     const user =  new UserUtil(User)
-    const {user_id} = await user.getById(1)
-    console.log(user_id)
+    const {user_id} = await user.getById(2)
     assert.isOk(user)
 })
   
+test("should return structured data with no references via post method.", async ({client,}) => {
+    const user = {
+        user_first_name : 'John',
+        user_last_name: 'Doe',
+        user_age : '18',
+        user_email: 'johndoeunqiue@gmail.com',
+        user_username: 'johndoe001unqiue',
+        user_password: '123456789',
+    }
+    const response = await client.post('http://127.0.0.1:4000/api/v1/users').send(user).end()
+    response.assertStatus(200)
+    response.assertJSONSubset({error: undefined,})
+    await User.find(response.body.data.user_id).then((response) =>response.delete())
+})
+
+test("should return structured data with no references via put method.", async ({client,}) => {
+    const user1 = {
+        user_first_name : 'John',
+        user_last_name: 'Doe',
+        user_age : '18',
+        user_email: 'johndoeunqiue01@gmail.com',
+        user_username: 'johndoe001unqiue',
+        user_password: '123456789',
+    }
+    const response1 = await client.post('http://127.0.0.1:4000/api/v1/users').send(user1).end()
+    console.log(response1.body.data.user_id)
+    const user2 = {
+        user_first_name : 'John',
+        user_last_name: 'Doe'
+    }
+    const response2 = await client.put(`http://127.0.0.1:4000/api/v1/users/${response1.body.data.user_id}`).send(user2).end()
+    response2.assertStatus(200)
+    response2.assertJSONSubset({error: undefined,})
+    await User.find(response2.body.data.user_id).then((response) =>response.delete())
+})
